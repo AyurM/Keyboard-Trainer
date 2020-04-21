9408f01202a116b702ca3164f79f8715cb85b6ba
AyurM <ayurmarh@gmail.com>
2020-04-21 17:31:59 +0800

    Добавлен переход на следующий уровень с повышением сложности

--- KeyboardTrainer/Program.cs
+++ KeyboardTrainer/Program.cs
@@ -5,59 +5,68 @@ namespace KeyboardTrainer {
         static void Main(string[] args) {
             ConsoleInit();
 
-            char[] symbols = { 'f', 'j', 'd', 'k', ' ' };
-            int totalSymbols = 50;
-            char[] type = new char[totalSymbols];
-            int symbolsOnScreen = 15;
-            int score = 0;
+            char[] symbols = { 'f', 'j', 'd', 'k', ' ', 'c', 'm', 'e', 'o', 'g', 'h', 's', 'l' };
+            int totalSymbols = 50;            
+            int symbolsOnScreen = 15;            
+            int level = 0;
+            int baseSymbols = 5;    //кол-во символов на 1м уровне
+            int symbolsPerLevel = 2;    //кол-во символов, добавляющихся на следующем уровне
 
             DateTime start, finish;
             Random rnd = new Random();
 
-            //Сформировать случайную последовательность из
-            //набора разрешенных символов
-            for (int i = 0; i < totalSymbols; i++) {
-                type[i] = symbols[rnd.Next(0, symbols.Length)];
-            }
-
-            start = DateTime.Now;
-
-            //Прокрутка последовательности на экране
-            for (int i = 0; i < totalSymbols; i++) {
-                WriteHead(score, i, totalSymbols);
-
-                //Выводится не более symbolsOnScreen символов за раз
-                for (int k = i; k < Math.Min(i + symbolsOnScreen - 1, totalSymbols); k++) {
-
-                    //Текущий символ выделяется цветом
-                    if (k == i)
-                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                    else
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                    //Символ пробела выводится как подчеркивание
-                    if (type[k] == ' ')
-                        Console.Write("_");
-                    else
-                        Console.Write(type[k]);
-
-                    Console.Write(" ");
+            while (level <= (symbols.Length - baseSymbols) / symbolsPerLevel) {
+                char[] type = new char[totalSymbols];
+                int score = 0;
+                //Сформировать случайную последовательность из
+                //набора разрешенных символов
+                for (int i = 0; i < totalSymbols; i++) {
+                    type[i] = symbols[rnd.Next(0, baseSymbols + level * symbolsPerLevel)];
                 }
 
-                //Ожидание нажатия клавиши
-                ConsoleKeyInfo key = Console.ReadKey(true);
+                start = DateTime.Now;
+               
+                //Прокрутка последовательности на экране
+                for (int i = 0; i < totalSymbols; i++) {
+                    WriteHead(score, i, totalSymbols);
 
-                //Правильная клавиша увеличивает счетчик очков
-                if (key.KeyChar.Equals(type[i]))
-                    score++;                
+                    //Выводится не более symbolsOnScreen символов за раз
+                    for (int k = i; k < Math.Min(i + symbolsOnScreen - 1, totalSymbols); k++) {
+
+                        //Текущий символ выделяется цветом
+                        if (k == i)
+                            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        else
+                            Console.ForegroundColor = ConsoleColor.Black;
+
+                        //Символ пробела выводится как подчеркивание
+                        if (type[k] == ' ')
+                            Console.Write("_");
+                        else
+                            Console.Write(type[k]);
+
+                        Console.Write(" ");
+                    }
+
+                    //Ожидание нажатия клавиши
+                    char pressedKey = Console.ReadKey(true).KeyChar;
+
+                    //Правильная клавиша увеличивает счетчик очков
+                    if (pressedKey == type[i])
+                        score++;
+                }
+
+                //Определить время, затраченное на прохождение
+                finish = DateTime.Now;
+                double totalTime = (finish - start).TotalSeconds;
+
+                Console.Clear();
+                ShowStats(level, score, totalSymbols, totalTime);
+
+                Console.ReadKey(true);
+                level++;
             }
 
-            //Определить время, затраченное на прохождение
-            finish = DateTime.Now;
-            double totalTime = (finish - start).TotalSeconds;
-
-            Console.Clear();
-            ShowStats(score, totalSymbols, totalTime);
             while (true) ;
         }
 
@@ -77,14 +86,14 @@ namespace KeyboardTrainer {
             Console.Write("\t\t");
         }
 
-        static void ShowStats(int score, int totalSymbols, double time) {
+        static void ShowStats(int level, int score, int totalSymbols, double time) {
             double accuracy = (double)score / totalSymbols;
 
             Console.WriteLine();
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.Black;
 
-            Console.Write("\t\tУровень пройден за ");
+            Console.Write("\t\tУровень " + (level + 1) +  " пройден за ");
 
             if (time <= 30.0)
                 Console.ForegroundColor = ConsoleColor.Green;
@@ -105,6 +114,10 @@ namespace KeyboardTrainer {
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
 
             Console.WriteLine(Math.Round(accuracy * 100) + "%");
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine();
+            Console.WriteLine("\t\tНажмите любую клавишу, чтобы перейти на следующий уровень");
         }
     }
 }
